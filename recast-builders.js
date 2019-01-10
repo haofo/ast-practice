33765acdb41f4cc21e179dce34b04f9527d200b2
haofo <897044545@qq.com>
2019-01-10 14:08:44 +0800

    refactor: add some comments

--- recast-builders.js
+++ recast-builders.js
@@ -14,32 +14,38 @@ const loop = b.forStatement(
 	b.emptyStatement()
 );
 
+// 变量声明
 const variable = b.variableDeclaration("let", [
 	b.variableDeclarator(b.identifier('a'), b.literal(1)),
 ], b.emptyStatement())
 
+// if 语句
 const fooId = b.identifier("foo");
 const ifFoo = b.ifStatement(fooId, b.blockStatement([
 	b.expressionStatement(b.callExpression(fooId, []))
 ]));
 
+// switch语句
 const SwitchStatement = b.switchStatement(fooId, [
 	b.switchCase(b.literal("1"), [
 		b.expressionStatement(b.updateExpression("++", b.identifier("i"), false)),
 		b.breakStatement()]),
 ])
 
+// return 语句
 const returnstatement = b.returnStatement(
 	b.binaryExpression(
 		"+", b.identifier("x"), b.literal(1)
 	)
 )
 
+// throw 语句
 const throwstatement = b.throwStatement(b.newExpression(
 	b.identifier('Error'),
 	[b.literal('抛出一个错误')]
 ))
 
+// try 语句
 const trystatement = b.tryStatement(
 	b.blockStatement([b.expressionStatement(b.callExpression(fooId, []))]),
 	b.catchClause(
@@ -49,6 +55,7 @@ const trystatement = b.tryStatement(
 	)
 );
 
+// while 语句
 const whilestatement = b.whileStatement(
 	b.binaryExpression("<", b.identifier('i'), b.literal(3)),
 	b.blockStatement(
@@ -56,6 +63,7 @@ const whilestatement = b.whileStatement(
 	)
 )
 
+// for...in
 const forinloop = b.forInStatement(
 	b.variableDeclaration("let", [
 		b.variableDeclarator(b.identifier("i"), null)
@@ -63,15 +71,20 @@ const forinloop = b.forInStatement(
 	b.identifier("obj"),
 	b.blockStatement([])
 );
+
+// this 表达式
 const thisexpression = b.thisExpression()
 
+// array
 const arrayExpress = b.arrayExpression([b.literal("a"), b.literal(1)])
 
+// object
 const objexpression = b.objectExpression([
 	b.property('init', b.identifier('a'), b.literal(1)),
 	b.property('init', b.identifier('b'), b.identifier('foo'))
 ])
 
+// function
 const FunctionExpression = b.functionExpression(b.identifier("succ"), [
 	b.identifier("x")
 ], b.blockStatement([
@@ -82,6 +95,7 @@ const FunctionExpression = b.functionExpression(b.identifier("succ"), [
 	)
 ]))
 
+// 一元运算符
 const unaryex = b.unaryExpression("!", b.sequenceExpression([
 	b.identifier("a"),
 	b.identifier("b"),
@@ -89,12 +103,14 @@ const unaryex = b.unaryExpression("!", b.sequenceExpression([
 ]))
 
 
+// 成员变量
 const member = b.memberExpression(
 	b.identifier("foo"),
 	b.identifier("bar"),
 	true
 )
 
+// 三元运算符表达式
 const condition = b.conditionalExpression(b.identifier('a'), b.identifier('b'), b.identifier('c'))
 
 const output = recast.print(member).code;
